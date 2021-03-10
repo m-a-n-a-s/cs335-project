58aca03195b35900be297c406432707d189bd8df
Sandeep <ssharma@iitk.ac.in>
2021-03-10 18:38:09 +0530

    structure of code updated

--- src/functions.h
+++ src/functions.h
@@ -8,6 +8,8 @@ struct node{
 };
 
 int find_id();
+struct node *square(char *str, struct node *node);
+struct node *parentheses(char *str, struct node *node);
 struct node *term_symb(char *str);
 struct node *non_term_symb(char *str,char *root, struct node *left, struct node *right);
 struct node *non_term_symb_1(char *str,char *root1, struct node *left,char *root2);
@@ -15,5 +17,3 @@ struct node *non_term_symb_2(char *str,struct node *left,struct node *m, struct 
 struct node *non_term_symb_3(char *str,char *root1,char *root3, struct node *left,char *root2);
 struct node *non_term_symb_4(char *str,struct node *node1,struct node *node2, struct node *node3, struct node*node4,char* root);
 struct node *non_term_symb_5(char *str,struct node *node1,struct node *node2, struct node *node3, struct node*node4,struct node *node5);
-struct node *parentheses(char *str, struct node *node);
-struct node *square(char *str, struct node *node);
--- test/sample.c
+++ test/sample.c
@@ -1,1 +1,7 @@
-int a = 2;
+int main(){
+	int a = 2;
+	int b = 3;
+	int c = a + b;
+	printf("%d",c);
+	return 0;
+}
